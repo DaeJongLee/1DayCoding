f99975a68b7ac571b6bea3aed96c6f51c4b255e5
daejonglee <rarongo@naver.com>
2024-02-09 15:24:15 +0900

    packaging and checking branching'

--- calculator.py
+++ calculator.py
@@ -1,12 +1,26 @@
+# taking number 
+input_ls = []
 input_a =input("첫번째 숫자를 입력하세요.:" )
+input_ls.append(input_a)
 input_b =input("두번째 숫자를 입력하세요.:" )
- 
-def calculater(input_a,input_b):
-    #checking input
-    print(f"첫번째 숫자는 {input_a}, 두번째 숫자는{input_b}입니다.")
-    input_how_to_calculate = input("연산자를 입력하세요 : + - * / ")
-    if input_how_to_calculate == "+":
-        print(num(input_a)+num(input_b))
+input_ls.append(input_b)
 
+#cheking number 
 
-calculater(input_b,input_b)
+print(f"첫번째 숫자는 {input_ls[0]}, 두번째 숫자는{input_ls[1]}입니다.")
+
+# calulating
+def calculater():
+    calculateing = input("연산자를 입력하세요 : + - * / ")
+    if calculateing == "+":
+        print(int(input_ls[0])+int(input_ls[1]))
+    elif calculateing == "-":         
+        print(int(input_ls[0])-int(input_ls[1]))
+    elif calculateing == "*":         
+        print(int(input_ls[0])*int(input_ls[1]))
+    elif calculateing == "/":         
+        print(int(input_ls[0])/int(input_ls[1]))
+
+#playing 
+            
+calculater()
