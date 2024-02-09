053874eeefa12a7854626b5ba79b2ac266f1d579
daejonglee <rarongo@naver.com>
2024-02-09 15:02:55 +0900

    두번째 10분input 을 연속으로 받는 과정에서 마지막값만 저장된다는것을 깨닫게 됨.

--- calculator.py
+++ calculator.py
@@ -2,6 +2,11 @@ input_a =input("첫번째 숫자를 입력하세요.:" )
 input_b =input("두번째 숫자를 입력하세요.:" )
  
 def calculater(input_a,input_b):
-    print(input_a)
+    #checking input
+    print(f"첫번째 숫자는 {input_a}, 두번째 숫자는{input_b}입니다.")
+    input_how_to_calculate = input("연산자를 입력하세요 : + - * / ")
+    if input_how_to_calculate == "+":
+        print(num(input_a)+num(input_b))
 
-calculater(input_a,input_b)
+
+calculater(input_b,input_b)
