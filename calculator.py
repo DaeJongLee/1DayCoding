8a19306ddd93e36823942b4bc00c7a41dfe5a99c
DaeJongLee <rarongo@naver.com>
2024-02-12 23:59:33 +0900

    2/12 오류값 잡기 완성

--- calculator.py
+++ calculator.py
@@ -1,26 +1,29 @@
-# taking number 
+#get the valulable numbers
+def get_number(prompt):
+    while True:
+        try: 
+            return float(input(prompt))
+        except ValueError:
+            print("유효한 숫자를 입력해주세요.")
+#taking number 
 input_ls = []
-input_a =input("첫번째 숫자를 입력하세요.:" )
-input_ls.append(input_a)
-input_b =input("두번째 숫자를 입력하세요.:" )
-input_ls.append(input_b)
-
-#cheking number 
-
-print(f"첫번째 숫자는 {input_ls[0]}, 두번째 숫자는{input_ls[1]}입니다.")
+for i in range(1,3):
+    input_number =get_number(f"{i}번째 숫자를 입력하세요.:" )
+    print (f"{i}번째 숫자는 {input_number},입니다.")
+    input_ls.append(input_number)
 
 # calulating
-def calculater():
+def perfrom_calculation():
     calculateing = input("연산자를 입력하세요 : + - * / ")
     if calculateing == "+":
-        print(int(input_ls[0])+int(input_ls[1]))
+        print(input_ls[0]+input_ls[1])
     elif calculateing == "-":         
-        print(int(input_ls[0])-int(input_ls[1]))
+        print((input_ls[0])-(input_ls[1]))
     elif calculateing == "*":         
-        print(int(input_ls[0])*int(input_ls[1]))
+        print((input_ls[0])*(input_ls[1]))
     elif calculateing == "/":         
-        print(int(input_ls[0])/int(input_ls[1]))
+        print((input_ls[0])/(input_ls[1]))
 
 #playing 
             
-calculater()
+perfrom_calculation()
